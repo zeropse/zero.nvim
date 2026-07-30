299988651425bc2a7cab0482ae0aac06cb5f2996
zeropse <deysrijit2@gmail.com>
2026-07-30 23:13:40 +0530

    feat: add GitHub Copilot AI completion, Copilot Chat, and Lua DAP extras

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,7 +19,10 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
+    { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.coding.copilot-chat" },
     { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.dap.nlua" },
     { import = "lazyvim.plugins.extras.editor.trouble" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.docker" },
