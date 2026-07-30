cd76c4499ee4be6205f7393d066226170b76b0f1
zeropse <deysrijit2@gmail.com>
2026-07-30 23:49:50 +0530

    fix: remove invalid editor.trouble extra import from lazy.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,7 +23,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ai.copilot-chat" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.dap.nlua" },
-    { import = "lazyvim.plugins.extras.editor.trouble" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.json" },
