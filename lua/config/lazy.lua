a734fbd806e3b6626f618c2abd6fa2af2736f09f
zeropse <deysrijit2@gmail.com>
2026-07-30 22:49:25 +0530

    feat: add vim-sleuth, trouble, and markdown extras

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,6 +19,8 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
+    { import = "lazyvim.plugins.extras.editor.trouble" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
