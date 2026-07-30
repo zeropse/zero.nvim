b8db75857055bae857e9c9522826fc56f83d9889
zeropse <deysrijit2@gmail.com>
2026-07-30 23:01:20 +0530

    feat: add autocommand to trim trailing whitespace on save

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,8 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
---
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Automatically trim trailing whitespace on save (preserving cursor position)
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = vim.api.nvim_create_augroup('trim_trailing_whitespace', { clear = true }),
+  pattern = '*',
+  callback = function()
+    local save_cursor = vim.fn.getpos('.')
+    vim.cmd([[%s/\s\+$//e]])
+    vim.fn.setpos('.', save_cursor)
+  end,
+})
